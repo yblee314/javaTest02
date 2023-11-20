c394625642b378735e18657e2b1cc7e4d09ff92e
yblee314 <yblee314@gmail.com>
2023-11-20 12:15:13 +0900

    update & new

--- src/javaTest02/testGit02.java
+++ src/javaTest02/testGit02.java
@@ -1,9 +1,6 @@
-// 업로드용 연습파일
-// 첫번째 수정
-
 package javaTest02;
 
-public class testGit01 {
+public class testGit02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
